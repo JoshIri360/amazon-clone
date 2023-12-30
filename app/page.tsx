9c469a57e68be68691d085c2bd64d6ca4d4fd87f
Joshua Aideloje <aidelojejoshua@gmail.com>
2023-12-30 20:00:11 +0100

    Update layout and remove unused components

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
-import LearnState from "@/components/LearnState";
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <LearnState />
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <div className="z-10 px-3 items-center justify-between font-mono text-sm lg:flex">
+        <p>Hello there</p>
       </div>
     </main>
   );
