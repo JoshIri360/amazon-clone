82641d5b6bf1e82e645871b20afaf5c0bbec34d0
Joshua Aideloje <aidelojejoshua@gmail.com>
2023-12-31 22:17:45 +0100

    Add ProductFeed component and fetch products from API

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,27 @@
 import ProductCarousel from "@/components/ProductCarousel";
+import ProductFeed from "@/components/ProductFeed";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <div className="responsive-width z-10 px-3 items-center justify-between font-mono text-sm lg:flex">
+      <div className="responsive-width z-10 px-3 items-center justify-between flex-col font-mono text-sm lg:flex">
         <div className="w-full">
           <ProductCarousel />
+        </div>
+        <div>
+          <ProductFeed />
         </div>
       </div>
     </main>
   );
 }
+
+export const getProducts = async () => {
+  const products = await fetch("https://fakestoreapi.com/products").then(
+    (res) => res.json()
+  );
+
+  console.log(products);
+
+  return products;
+};
--- components/ProductFeed.tsx
+++ components/ProductFeed.tsx
@@ -1,7 +1,19 @@
-function ProductFeed() {
+import { getProducts } from "@/app/page";
+
+async function ProductFeed() {
+  const products = await getProducts();
+
+  console.log(products);
   return (
-    <div>Products here...</div>
-  )
+    <div>
+      <div>Products here...</div>
+      <div>
+        {products.map((product: any) => (
+          <p key={product.id}>{product.title}</p>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default ProductFeed
+export default ProductFeed;
