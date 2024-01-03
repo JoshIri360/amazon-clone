5e73cf39a58e2571c23eec365f79e7f0da4d8e38
Joshua Aideloje <aidelojejoshua@gmail.com>
2024-01-03 17:40:09 +0100

    Refactor page and product feed components

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import ProductCarousel from "@/components/ProductCarousel";
 import ProductFeed from "@/components/ProductFeed";
+import { Inter } from "next/font/google";
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <div className="responsive-width z-10 px-3 items-center justify-between flex-col font-mono text-sm lg:flex">
+    <main
+      className={`${inter.className} flex min-h-screen flex-col items-center justify-between`}
+    >
+      <div className="responsive-width z-10 px-3 items-center justify-between flex-col text-sm lg:flex">
         <div className="w-full">
           <ProductCarousel />
         </div>
@@ -20,8 +24,6 @@ export const getProducts = async () => {
   const products = await fetch("https://fakestoreapi.com/products").then(
     (res) => res.json()
   );
-
-  console.log(products);
 
   return products;
 };
--- components/ProductFeed.tsx
+++ components/ProductFeed.tsx
@@ -1,16 +1,41 @@
 import { getProducts } from "@/app/page";
+import Image from "next/image";
+import { Key } from "react";
+
+type productProps = {
+  id: Key;
+  title: string;
+  price: string;
+  description: string;
+  category: string;
+  image: string;
+};
 
 async function ProductFeed() {
   const products = await getProducts();
 
-  console.log(products);
   return (
     <div>
       <div>Products here...</div>
-      <div>
-        {products.map((product: any) => (
-          <p key={product.id}>{product.title}</p>
-        ))}
+      <div className="grid grid-cols-4 gap-10 w-full">
+        {products.map(
+          ({
+            id,
+            title,
+            price,
+            description,
+            category,
+            image,
+          }: productProps) => (
+            <div key={id} className="flex-col gap-4 border border-blue-400 p-4">
+              <Image src={image} alt={title} width={200} height={200} />
+
+              <h3>{title}</h3>
+              <p>{price}</p>
+              <p>{description}</p>
+            </div>
+          )
+        )}
       </div>
     </div>
   );
