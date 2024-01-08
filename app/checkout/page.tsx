c8ec66a11d61c7fcea9a63c103334ac43784c72e
Joshua Aideloje <aidelojejoshua@gmail.com>
2024-01-08 19:36:09 +0100

    Refactor checkout page and remove unused carousel images

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,11 +1,41 @@
-const page = () => {
+"use client";
+import React, { useState, useEffect } from "react";
+import Image from "next/image";
+
+const Page = () => {
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    // Update the state on window resize
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth < 600);
+    };
+
+    // Attach event listener
+    window.addEventListener("resize", handleResize);
+
+    // Call the handler right away so state gets updated with initial window size
+    handleResize();
+
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("resize", handleResize);
+  }, []); // Empty array ensures that effect is only run on mount and unmount
+
   return (
     <div className="flex-center w-screen">
-      <div className="responsive-width">
-        Checkout page
+      {/* Left */}
+      <div className="w-[min(90%,1300px)]">
+        <Image
+          src={isSmallScreen ? "/sm/cart.jpg" : "/lg/cart.jpg"}
+          alt="Cart"
+          width={1020}
+          height={400}
+        />
       </div>
-    </div>
-  )
-}
 
-export default page
+      {/* Right */}
+    </div>
+  );
+};
+
+export default Page;
