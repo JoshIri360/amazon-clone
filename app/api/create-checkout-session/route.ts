c6885b082f567a058d579d1825f5ae2fdba35a48
Joshua Aideloje <aidelojejoshua@gmail.com>
2024-01-10 09:38:28 +0100

    Add express dependency and update create-checkout-session route

--- app/api/create-checkout-session/route.ts
+++ app/api/create-checkout-session/route.ts
@@ -2,22 +2,37 @@ import { Request, Response } from "express";
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 export const POST = async (req: Request, res: Response) => {
-  console.log(await req.json());
+  try {
+    const { cart, email } = await req.json();
 
-  const transformedItems = req.body.map((item: any) => ({
-    description: item.description,
-    quantity: item.quantity,
-    price_data: {
-      currency: "gbp",
-      unit_amount: item.price * 100,
-      product_data: {
-        name: item.title,
-        images: [item.image],
-      },
-    },
-  }));
+    const transformedItems = cart.map((item: any) => {
+      return {
+        description: item.description,
+        quantity: item.quantity,
+        price_data: {
+          currency: "gbp",
+          unit_amount: item.price * 100,
+          product_data: {
+            name: item.title,
+            images: [item.image],
+          },
+        },
+      };
+    });
 
-  const session = await stripe.checkout.sessions.create({
-    
-  })
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: [...transformedItems],
+      mode: "payment",
+      success_url: `${process.env.HOST}/success`,
+      cancel_url: `${process.env.HOST}/checkout`,
+    });
+
+    return new Response(JSON.stringify({ id: session.id }), {
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    console.log(error);
+    return new Response(error.message, { status: 500 });
+  }
 };
