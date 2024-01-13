6b530def55e44b45af1c5458bc9d5d5d363d1bfc
Joshua Aideloje <aidelojejoshua@gmail.com>
2024-01-13 15:48:48 +0100

    ➕Add image display for each item in orders

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -4,6 +4,21 @@ import { useSession } from "next-auth/react";
 import React, { useEffect, useState } from "react";
 import { Query, DocumentData } from "@firebase/firestore-types";
 import { collection, getDocs, orderBy, query } from "firebase/firestore";
+import Image from "next/image";
+
+interface Order {
+  total: number;
+  timestamp: string;
+  id: string;
+  items: Item[];
+}
+
+interface Item {
+  description: string;
+  amount_total: number;
+  quantity: number;
+  images: string[];
+}
 
 const Orders = () => {
   const { data: session } = useSession();
@@ -28,7 +43,39 @@ const Orders = () => {
             <h2 className="text-2xl font-semibold">
               {session?.user?.name}&apos;s Orders
             </h2>
-            <div>{orders}</div>
+            <div className="flex flex-col flex-center">
+              {orders.map((order: Order) => {
+                return (
+                  <div key={order.id} className="w-[min(95%,850px)]">
+                    <div className="grid grid-cols-3">
+                      <p>Order Date</p>
+                      <p>Order #</p>
+                      <p>Total</p>
+                      <p>{order.timestamp}</p>
+                      <p>{order.id}</p>
+                      <p>{order.total.toFixed(2)}</p>
+                    </div>
+                    <div>
+                      {order.items.map((item: Item) => {
+                        return (
+                          <div key={item.description}>
+                            <h2>{item.description}</h2>
+                            <p>£{item.amount_total}</p>
+                            <p>{item.quantity}</p>
+                            <Image
+                              src={item.images[0]}
+                              alt={item.description}
+                              width={150}
+                              height={150}
+                            />
+                          </div>
+                        );
+                      })}
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
           </div>
         ) : (
           <h2 className="text-2xl font-semibold">
@@ -47,18 +94,17 @@ const getOrders = async (session: any) => {
   const querySnapshot = await getDocs(q);
 
   querySnapshot.forEach((doc) => {
-    const date = new Date(doc.data());
+    const date = new Date(doc.data().timeStamp * 1000);
     const dateString = date.toDateString();
     console.log(doc.id, " => ", doc.data());
     orders.push({
       ...doc.data(),
       timestamp: dateString,
+      id: doc.id,
     });
   });
 
-  console.log(orders);
-
-  return;
+  return orders;
 };
 
 export default Orders;
